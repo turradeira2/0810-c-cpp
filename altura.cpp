2a92e70fe317eeed9582c58e5e4da7d99a6da98e
turradeira2 <tiagaomatos@hotmail.com>
2024-12-02 16:43:44 +0000

    Create altura.cpp

---
+++ altura.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age;
+    float alt;
+    cout << "isert age:";
+    cin >> age;
+
+    cout << "isert height:";
+    cin >> alt;
+
+    cout << "age ="<< age << " height ="<< height << endl;
+
+    return 0;
+}
